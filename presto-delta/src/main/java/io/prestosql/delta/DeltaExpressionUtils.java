7eb7c9afb019217f5eb09c3bebf1165e2e7f0f03
Venki Korukanti <venki.korukanti@gmail.com>
2021-09-27 14:03:25 -0700

    Delta lake connector - reader

---
+++ presto-delta/src/main/java/io/prestosql/delta/DeltaExpressionUtils.java
@@ -0,0 +1,243 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.delta;
+
+import com.google.common.collect.ImmutableList;
+import io.airlift.slice.Slice;
+import io.delta.standalone.actions.AddFile;
+import io.prestosql.spi.PrestoException;
+import io.prestosql.spi.connector.ColumnHandle;
+import io.prestosql.spi.connector.ConnectorSession;
+import io.prestosql.spi.predicate.Domain;
+import io.prestosql.spi.predicate.TupleDomain;
+import io.prestosql.spi.predicate.ValueSet;
+import io.prestosql.spi.type.StandardTypes;
+import io.prestosql.spi.type.Type;
+import io.prestosql.spi.type.TypeManager;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static io.airlift.slice.Slices.utf8Slice;
+import static io.prestosql.delta.DeltaColumnHandle.ColumnType.PARTITION;
+import static io.prestosql.delta.DeltaErrorCode.DELTA_INVALID_PARTITION_VALUE;
+import static io.prestosql.delta.DeltaErrorCode.DELTA_UNSUPPORTED_COLUMN_TYPE;
+import static io.prestosql.delta.DeltaSessionProperties.isPartitionPruningEnabled;
+import static java.lang.Double.doubleToRawLongBits;
+import static java.lang.Double.parseDouble;
+import static java.lang.Float.floatToRawIntBits;
+import static java.lang.Float.parseFloat;
+import static java.lang.Long.parseLong;
+import static java.lang.String.format;
+import static java.util.stream.Collectors.toMap;
+
+public class DeltaExpressionUtils
+{
+    private DeltaExpressionUtils()
+    {
+    }
+
+    /**
+     * Split the predicate into partition and regular column predicates
+     */
+    public static List<TupleDomain<ColumnHandle>> splitPredicate(
+            TupleDomain<ColumnHandle> predicate)
+    {
+        Map<ColumnHandle, Domain> partitionColumnPredicates = new HashMap<>();
+        Map<ColumnHandle, Domain> regularColumnPredicates = new HashMap<>();
+
+        Optional<Map<ColumnHandle, Domain>> domains = predicate.getDomains();
+        if (domains.isPresent()) {
+            domains.get().entrySet().stream()
+                    .forEach(domainPair -> {
+                        DeltaColumnHandle columnHandle = (DeltaColumnHandle) domainPair.getKey();
+                        if (columnHandle.getColumnType() == PARTITION) {
+                            partitionColumnPredicates.put(domainPair.getKey(), domainPair.getValue());
+                        }
+                        else {
+                            regularColumnPredicates.put(domainPair.getKey(), domainPair.getValue());
+                        }
+                    });
+        }
+
+        return ImmutableList.of(
+                TupleDomain.withColumnDomains(partitionColumnPredicates),
+                TupleDomain.withColumnDomains(regularColumnPredicates));
+    }
+
+    /**
+     * Utility method to
+     * 1) remove the null value partition values. These null values cause problems later
+     *    when used with Guava Immutable map structures.
+     * 2) convert column names to lower case. Presto converts the column names to lowercase.
+     */
+    public static Map<String, String> sanitizePartitionValues(Map<String, String> partitionValues)
+    {
+        return partitionValues.entrySet().stream()
+                .filter(entry -> entry.getValue() != null)
+                .collect(toMap(entry -> entry.getKey().toLowerCase(Locale.ROOT), Map.Entry::getValue));
+    }
+
+    /**
+     * Utility method that takes an iterator of {@link AddFile}s and a predicate and returns an iterator of {@link AddFile}s
+     * that satisfy the predicate (predicate evaluates to a deterministic NO)
+     */
+    public static Iterator<AddFile> iterateWithPredicate(
+            ConnectorSession session,
+            Iterator<AddFile> inputIter,
+            TupleDomain<DeltaColumnHandle> predicate,
+            TypeManager typeManager)
+    {
+        TupleDomain<String> partitionPredicate = extractPartitionColumnsPredicate(predicate);
+        if (partitionPredicate.isAll() /* no partition filter */ || !isPartitionPruningEnabled(session)) {
+            return inputIter;
+        }
+
+        if (partitionPredicate.isNone()) {
+            return Collections.emptyIterator(); // nothing passes the partition predicate, return empty iterator
+        }
+
+        List<DeltaColumnHandle> partitionColumns =
+                predicate.getColumnDomains().get().stream()
+                        .filter(entry -> entry.getColumn().getColumnType() == PARTITION)
+                        .map(entry -> entry.getColumn())
+                        .collect(Collectors.toList());
+
+        return new Iterator<AddFile>()
+        {
+            private AddFile nextItem;
+
+            @Override
+            public boolean hasNext()
+            {
+                if (nextItem != null) {
+                    return true;
+                }
+
+                while (inputIter.hasNext()) {
+                    AddFile nextFile = inputIter.next();
+                    if (evaluatePartitionPredicate(partitionPredicate, partitionColumns, typeManager, nextFile)) {
+                        nextItem = nextFile;
+                        break;
+                    }
+                }
+
+                return nextItem != null;
+            }
+
+            @Override
+            public AddFile next()
+            {
+                if (!hasNext()) {
+                    throw new NoSuchElementException("there are no more files");
+                }
+                AddFile toReturn = nextItem;
+                nextItem = null;
+                return toReturn;
+            }
+        };
+    }
+
+    private static TupleDomain<String> extractPartitionColumnsPredicate(TupleDomain<DeltaColumnHandle> predicate)
+    {
+        return predicate.transform(
+                deltaColumnHandle -> {
+                    if (deltaColumnHandle.getColumnType() != PARTITION) {
+                        return null;
+                    }
+                    return deltaColumnHandle.getName();
+                });
+    }
+
+    private static boolean evaluatePartitionPredicate(
+            TupleDomain<String> partitionPredicate,
+            List<DeltaColumnHandle> partitionColumns,
+            TypeManager typeManager,
+            AddFile addFile)
+    {
+        Map<String, String> partitionValues = sanitizePartitionValues(addFile.getPartitionValues());
+        for (DeltaColumnHandle partitionColumn : partitionColumns) {
+            String columnName = partitionColumn.getName();
+            String partitionValue = partitionValues.get(columnName.toLowerCase(Locale.ROOT));
+            Domain domain = getDomain(partitionColumn, partitionValue, typeManager, addFile.getPath());
+            Domain columnPredicate = partitionPredicate.getDomains().get().get(columnName);
+
+            if (columnPredicate == null) {
+                continue; // there is no predicate on this column
+            }
+
+            if (columnPredicate.intersect(domain).isNone()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static Domain getDomain(DeltaColumnHandle columnHandle, String partitionValue, TypeManager typeManager, String filePath)
+    {
+        Type type = typeManager.getType(columnHandle.getDataType());
+        if (partitionValue == null) {
+            return Domain.onlyNull(type);
+        }
+
+        String typeBase = columnHandle.getDataType().getBase();
+        try {
+            switch (typeBase) {
+                case StandardTypes.TINYINT:
+                case StandardTypes.SMALLINT:
+                case StandardTypes.INTEGER:
+                case StandardTypes.BIGINT:
+                    Long intValue = parseLong(partitionValue);
+                    return Domain.create(ValueSet.of(type, intValue), false);
+                case StandardTypes.REAL:
+                    Long realValue = (long) floatToRawIntBits(parseFloat(partitionValue));
+                    return Domain.create(ValueSet.of(type, realValue), false);
+                case StandardTypes.DOUBLE:
+                    Long doubleValue = doubleToRawLongBits(parseDouble(partitionValue));
+                    return Domain.create(ValueSet.of(type, doubleValue), false);
+                case StandardTypes.VARCHAR:
+                case StandardTypes.VARBINARY:
+                    Slice sliceValue = utf8Slice(partitionValue);
+                    return Domain.create(ValueSet.of(type, sliceValue), false);
+                case StandardTypes.DATE:
+                    Long dateValue = Date.valueOf(partitionValue).toLocalDate().toEpochDay();
+                    return Domain.create(ValueSet.of(type, dateValue), false);
+                case StandardTypes.TIMESTAMP:
+                    Long timestampValue = Timestamp.valueOf(partitionValue).getTime(); // convert to millis
+                    return Domain.create(ValueSet.of(type, timestampValue), false);
+                case StandardTypes.BOOLEAN:
+                    Boolean booleanValue = Boolean.valueOf(partitionValue);
+                    return Domain.create(ValueSet.of(type, booleanValue), false);
+                default:
+                    throw new PrestoException(DELTA_UNSUPPORTED_COLUMN_TYPE,
+                            format("Unsupported data type '%s' for partition column %s", columnHandle.getDataType(), columnHandle.getName()));
+            }
+        }
+        catch (IllegalArgumentException e) {
+            throw new PrestoException(DELTA_INVALID_PARTITION_VALUE,
+                    format("Can not parse partition value '%s' of type '%s' for partition column '%s' in file '%s'",
+                            partitionValue, columnHandle.getDataType(), columnHandle.getName(), filePath));
+        }
+    }
+}
---
+++ presto-delta/src/main/java/io/prestosql/delta/DeltaSplitManager.java
@@ -0,0 +1,121 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prestosql.delta;
+
+import io.delta.standalone.actions.AddFile;
+import io.prestosql.spi.connector.ConnectorPartitionHandle;
+import io.prestosql.spi.connector.ConnectorSession;
+import io.prestosql.spi.connector.ConnectorSplit;
+import io.prestosql.spi.connector.ConnectorSplitManager;
+import io.prestosql.spi.connector.ConnectorSplitSource;
+import io.prestosql.spi.connector.ConnectorTableHandle;
+import io.prestosql.spi.connector.ConnectorTransactionHandle;
+import io.prestosql.spi.type.TypeManager;
+import org.apache.hadoop.fs.Path;
+
+import javax.inject.Inject;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import static io.prestosql.delta.DeltaExpressionUtils.sanitizePartitionValues;
+import static java.util.Objects.requireNonNull;
+import static java.util.concurrent.CompletableFuture.completedFuture;
+
+public class DeltaSplitManager
+        implements ConnectorSplitManager
+{
+    private final String connectorId;
+    private final DeltaConfig deltaConfig;
+    private final DeltaClient deltaClient;
+    private final TypeManager typeManager;
+
+    @Inject
+    public DeltaSplitManager(
+            DeltaConnectorId connectorId,
+            DeltaConfig deltaConfig,
+            DeltaClient deltaClient,
+            TypeManager typeManager)
+    {
+        this.connectorId = requireNonNull(connectorId, "connectorId is null").toString();
+        this.deltaConfig = requireNonNull(deltaConfig, "deltaConfig is null");
+        this.deltaClient = requireNonNull(deltaClient, "deltaClient is null");
+        this.typeManager = requireNonNull(typeManager, "typeManager is null");
+    }
+
+    @Override
+    public ConnectorSplitSource getSplits(
+            ConnectorTransactionHandle transaction,
+            ConnectorSession session,
+            ConnectorTableHandle table,
+            SplitSchedulingStrategy splitSchedulingStrategy)
+    {
+        DeltaTableHandle tableHandle = (DeltaTableHandle) table;
+        return new DeltaSplitSource(session, tableHandle);
+    }
+
+    private class DeltaSplitSource
+            implements ConnectorSplitSource
+    {
+        private final DeltaTable deltaTable;
+        private final Iterator<AddFile> fileIter;
+        private final int maxBatchSize;
+
+        DeltaSplitSource(ConnectorSession session, DeltaTableHandle deltaTableHandle)
+        {
+            this.deltaTable = deltaTableHandle.getDeltaTable();
+            this.fileIter = DeltaExpressionUtils.iterateWithPredicate(
+                    session,
+                    deltaClient.listFiles(session, deltaTable),
+                    deltaTableHandle.getPredicate(),
+                    typeManager);
+            this.maxBatchSize = deltaConfig.getMaxSplitsBatchSize();
+        }
+
+        @Override
+        public CompletableFuture<ConnectorSplitBatch> getNextBatch(ConnectorPartitionHandle partitionHandle, int maxSize)
+        {
+            List<ConnectorSplit> splits = new ArrayList<>();
+            while (fileIter.hasNext() && splits.size() < maxSize && splits.size() < maxBatchSize) {
+                AddFile file = fileIter.next();
+                Path filePath = new Path(deltaTable.getTableLocation(), URI.create(file.getPath()).getPath());
+                splits.add(new DeltaSplit(
+                        connectorId,
+                        deltaTable.getSchemaName(),
+                        deltaTable.getTableName(),
+                        filePath.toString(),
+                        0, /* start */
+                        file.getSize() /* split length - read the entire file in one split */,
+                        file.getSize(),
+                        sanitizePartitionValues(file.getPartitionValues())));
+            }
+
+            return completedFuture(new ConnectorSplitBatch(splits, !fileIter.hasNext()));
+        }
+
+        @Override
+        public void close()
+        {
+        }
+
+        @Override
+        public boolean isFinished()
+        {
+            return !fileIter.hasNext();
+        }
+    }
+}
